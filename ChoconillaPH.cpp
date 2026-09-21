e5dba8c12000c7c8a76399ba1fa56f1f6502503f
Fregoso-Edgar <edgarfregoso57@gmail.com>
2026-09-21 16:26:41 -0600

    Refactor Ser class constructors and methods
    Updated class 'Ser' to fix constructor and member functions.

--- ChoconillaPH.cpp
+++ ChoconillaPH.cpp
@@ -15,111 +15,74 @@ private:
 	
 public:
     Ser(){ //Constructor por defecto
-		nombre="Nadie"; max_hp=50; max_eng=30; atk=10; vivo=true;
-        setHp(50);
-        setEnergia(30);
+		nombre="Nadie"; max_hp=50; hp=50; max_eng=30; energia=30; atk=10; vivo=true;
 	}
 	
 	Ser(string n, int maxh, int h, int maxe, int e, int a, bool v){ //Constructor por parametros
 		nombre=n;
 		max_hp=maxh;
+		hp=h;
 		max_eng=maxe;
+		energia=e;
 		atk=a;
 		vivo=v;
-        setHp(h);
-        setEnergia(e);
 	}
 	
     ~Ser(){ //Destructor
-        cout << "[Destruido] " << nombre << " se libero de la memoria." << endl;
 	}
 	
-    void setHp(int h) {
-        if (h < 0) h = 0;
-        if (h > max_hp) h = max_hp;
-        hp = h;
-        if (hp == 0) vivo = false;
-    }
-    int getHp() const { return hp; }
-
-    void setEnergia(int e) {
-        if (e < 0) e = 0;
-        if (e > max_eng) e = max_eng;
-        energia = e;
-    }
-    int getEnergia() const { return energia; }
-    
-    string getNombre() const { return nombre; }
-
 	//FUNCIONES
-	void mostrar(){ //Mirar datos
-	    cout << " | Nombre: " << nombre << " | Vida: " << max_hp << "/" << hp 
-             << " | Estamina: " << max_eng << "/" << energia << " | Ataque : " << atk << endl;
+	void mostrar(){//Mirar datos
+	cout << " | Nombre: " << nombre << " | Vida: " << max_hp << "/" << hp << " | Estamina: " << max_eng << "/" << energia << " | Ataque : " << atk << endl;
+	cout << endl;
 	}
 	
-    void atacar(Ser& obj){ //atacar
-        int nueva_vida = obj.getHp() - atk; 
-        obj.setHp(nueva_vida);
-        cout << obj.getNombre() << " recibio " << atk << " puntos de danio por parte de " << nombre << endl;
-    }
-
-    void curar() { 
-        cout << nombre << " usa curacion basica (+10 de vida)." << endl;
-        setHp(hp + 10);
-    }
-
-    void curar(int cantidad) { 
-        cout << nombre << " usa una pocion y se cura por " << cantidad << " puntos de vida." << endl;
-        setHp(hp + cantidad);
-    }
+		void atacar(Ser& obj){ //atacar
+ 	     int vida;
+ 	     vida = obj.hp - atk; 
+ 	     obj.hp = vida;
+ 	     if(obj.hp<=0){obj.hp=0;}
+ 	     cout << obj.nombre << " recibio " << atk << " puntos de danio por parte de " << nombre << endl;
+		}
 		
-    void rest(){ // descansar
-        int tiempo;
-        cout << "¿Por cuanto tiempo descansa " << nombre << "?" << endl;
-        cin >> tiempo;
-        setEnergia(energia + (tiempo*2));
-    } 
+		void rest(){ // descansar
+		int tiempo;
+				
+		    cout << "¿Por cuanto tiempo descansa " << nombre << "?" << endl;
+		    cin >> tiempo;
+		    energia = energia + (tiempo*2);
+		    if (energia > max_eng){energia = max_eng;}
+		} 
 		
-    void correr(){ // perder energia
-        int tiempo;
-        cout << "¿Por cuanto tiempo corre " << nombre << "?" << endl;
-        cin >> tiempo;
-        setEnergia(energia - (tiempo*2));
-    } 
+		void correr(){ // perder energia
+		int tiempo;
+		
+		    cout << "¿Por cuanto tiempo corre " << nombre << "?" << endl;
+			cin >> tiempo;
+			
+			energia = energia - (tiempo*2);
+			if (energia<0){energia=0;}
+		} 
+	
 };
 	
+
 int main(int argc, char** argv) {
 	
-	Ser* Roedor = new Ser("Rata", 20, 20, 50, 50, 100, true);
+	Ser Roedor("Rata", 20, 20, 50, 50, 100, true);
     Ser Who;
 
-    Roedor->mostrar();
+    Roedor.mostrar();
     Who.mostrar();
     
-    /*Roedor->correr();
-    Roedor->mostrar();
+    /*Roedor.correr();
+    Roedor.mostrar();
     
-    Roedor->rest();
-    Roedor->mostrar();*/
-
-    cout << "\n--- Prueba ---" << endl;
-    Roedor->setHp(-999);
-    Roedor->mostrar();
+    Roedor.rest();
+    Roedor.mostrar();*/
     
-    Roedor->setHp(20);
-
-    cout << "\n--- Combate ---" << endl;
-    Roedor->atacar(Who);
+    Roedor.atacar(Who);
     Who.mostrar();
-    
-    Who.curar();       
-    Who.mostrar();
-
-    Who.curar(20);     
-    Who.mostrar();
-    cout << endl;
-    
-    delete Roedor;
     
 	return 0;
 }
